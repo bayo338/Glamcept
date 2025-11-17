641001d370bb6f5e335f3de55bd95250542c8a55
igbaroola samuel adebayo <igbaroola338@gmail.com>
2025-11-17 05:42:12 +0100

    vercel edit

--- app/logout/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from 'next/server';
-
-// export async function GET() {
-//   const res = NextResponse.redirect('/');
-//   res.cookies.set('token', '', { maxAge: 0 });
-//   return res;
-// }
